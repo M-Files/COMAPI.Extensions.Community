ae2be216a802746446ed26093331c3024e56e1b7
Craig Hawker <craig.hawker@gmail.com>
2020-02-28 09:24:36 +0000

    Added TryGetObjectClassNamePropertyDef. Added unit tests for above.

---
+++ MFilesAPI.Extensions.Tests/ExtensionMethods/VaultClassOperations/TryGetObjectClassNamePropertyDef.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MFilesAPI.Extensions.Tests.ExtensionMethods.VaultClassOperations
+{
+	[TestClass]
+	public class TryGetObjectClassNamePropertyDef
+		: VaultClassOperationsTestBase
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NullReferenceThrows()
+		{
+			((MFilesAPI.VaultClassOperations) null).TryGetObjectClassNamePropertyDef(1, out _);
+		}
+
+		[TestMethod]
+		[DataRow(1234, 0)]
+		[DataRow(1234, 1020)]
+		public void KnownObjectClassReturnsCorrectData
+			(
+			int classId,
+			int expectedPropertyDefId
+			)
+		{
+			// Mock the class.
+			var mock = this.GetVaultClassOperationsMock();
+			mock
+				.Setup(m => m.GetObjectClass(classId))
+				.Returns((int id) =>
+				{
+					var objectClass = new ObjectClass()
+					{
+						ID = classId,
+						NamePropertyDef = expectedPropertyDefId
+					};
+					return objectClass;
+				});
+			
+			// Run the method and check the output.
+			Assert.IsTrue(mock.Object.TryGetObjectClassNamePropertyDef(classId, out int output));
+			Assert.AreEqual(expectedPropertyDefId, output);
+		}
+
+		[TestMethod]
+		public void UnknownObjectClassReturnsFalse()
+		{
+			// Details about the class.
+			int classId = 1234;
+			
+			// Mock the class.
+			var mock = this.GetVaultClassOperationsMock();
+			mock
+				.Setup(m => m.GetObjectClass(classId))
+				.Returns((int id) => throw new InvalidOperationException("Sample error"));
+			
+			// Run the method and check the output.
+			Assert.IsFalse(mock.Object.TryGetObjectClassNamePropertyDef(classId, out _));
+		}
+	}
+}
---
+++ MFilesAPI.Extensions/ExtensionMethods/VaultClassOperations/TryGetObjectClassNamePropertyDef.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MFilesAPI.Extensions
+{
+	public static partial class VaultClassOperationsExtensionMethods
+	{
+		/// <summary>
+		/// Attempts to get the ID of the <see cref="PropertyDef"/> that is defined as
+		/// the "Name or Title" property of a <see cref="ObjectClass"/>
+		/// with the given <paramref name="classId"/>.
+		/// </summary>
+		/// <param name="classOperations">The <see cref="VaultClassOperations"/> instance to use to communicate with the vault.</param>
+		/// <param name="classId">The Id of the class to attempt to read.</param>
+		/// <param name="nameOrTitlePropertyDef">The property of the "Name or Title" property for this class, or <see cref="MFBuiltInPropertyDef.MFBuiltInPropertyDefNameOrTitle"/> if not set.</param>
+		/// <returns>true if the class could be found, false otherwise.</returns>
+		public static bool TryGetObjectClassNamePropertyDef
+		(
+			this VaultClassOperations classOperations,
+			int classId,
+			out int nameOrTitlePropertyDef
+		)
+		{
+			// Sanity.
+			if (null == classOperations)
+				throw new ArgumentNullException(nameof(classOperations));
+
+			// Default.
+			nameOrTitlePropertyDef = (int)MFBuiltInPropertyDef.MFBuiltInPropertyDefNameOrTitle;
+
+			// Attempt to get from the underlying data.
+			try
+			{
+				nameOrTitlePropertyDef = classOperations
+					.GetObjectClass(classId)
+					.NamePropertyDef;
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
