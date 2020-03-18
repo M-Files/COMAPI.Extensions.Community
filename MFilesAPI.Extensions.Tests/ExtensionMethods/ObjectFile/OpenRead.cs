32ac10837d33f19b22abf6f2abf397ac7f3d91b1
Craig Hawker <craig.hawker@gmail.com>
2020-03-18 17:35:08 +0000

    Added tests for OpenRead

---
+++ MFilesAPI.Extensions.Tests/ExtensionMethods/ObjectFile/OpenRead.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MFilesAPI.Extensions.Tests.Stubs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MFilesAPI.Extensions.Tests.ExtensionMethods.ObjectFile
+{
+	[TestClass]
+	public class OpenRead
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void OpenRead_ObjectFile()
+		{
+			((MFilesAPI.ObjectFile) null)
+				.OpenRead
+				(
+					Moq.Mock.Of<Vault>()
+				);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void OpenReadThrowsException_Vault()
+		{
+			(new ObjectFileStub())
+				.OpenRead
+					(
+						vault: null
+					);
+		}
+	}
+}
